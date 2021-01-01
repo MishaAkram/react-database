b02269764ad693da421ea6863a6c422c8c3d4066
MishaAkram <mishaakrambaig@gmail.com>
2021-01-02 00:16:34 +0500

    header/ footer added

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="pt-5">
+      <div className="container text-center">
+        <p>&copy; ABC 2020. All rights reserved.</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer;
