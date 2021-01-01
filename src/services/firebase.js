75bca8e9874ed5418270da661456dc0b865b4f3f
MishaAkram <mishaakrambaig@gmail.com>
2021-01-02 00:21:01 +0500

    firebase added

---
+++ src/services/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+const config = {
+ 
+  apiKey: "AIzaSyAy09yXdbLgOqAmTeEXCAZnzIDEgmPaKjY",
+  authDomain: "database-41842.firebaseapp.com",
+  databaseURL: "https://database-41842-default-rtdb.firebaseio.com",
+  
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth;
