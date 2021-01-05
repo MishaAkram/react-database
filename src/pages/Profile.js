92978a98445fcaa0ac27d0360b1276687a48b6e3
SoniyaShafi <aa9391438@gmail.com>
2021-01-05 22:02:46 +0500

    changes

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -7,17 +7,112 @@ export default class Profile extends Component {
   constructor() {
     super();
     this.state = {
-      user: auth().currentUser
+      user: auth().currentUser,
+      notes: [],
+      content: "",
+      note: {}
     };
+    this.handleChange = this.handleChange.bind(this);
+    this.createNote = this.createNote.bind(this);
+    this.editNote = this.editNote.bind(this);
   }
 
-  componentDidMount() {}
+  componentDidMount() {
+    db.ref(`all_notes/${this.state.user.uid}`).on("value", snapshot => {
+      let allNotes = [];
+      snapshot.forEach(snap => {
+        allNotes.push(snap.val());
+      });
+      console.log(allNotes);
+      this.setState({ notes: allNotes });
+    });
+  }
+
+  handleChange(e) {
+    this.setState({
+      content: e.target.value
+    });
+  }
+
+  createNote() {
+    const uid = this.state.user.uid;
+    const { content } = this.state;
+    const note = this.state.note;
+    if (note && note.content) {
+      return db
+        .ref(`all_notes/${uid}/${note.note_id}`)
+        .update({
+          content,
+          uid
+        })
+        .then(_ => {
+          this.setState({ content: "", note: {} });
+        })
+        .catch(error => console.log(error.message));
+    }
+    const note_id = `note-${Date.now()}`;
+    db.ref(`all_notes/${uid}/${note_id}`)
+      .set({
+        content,
+        note_id,
+        uid
+      })
+      .then(_ => {
+        this.setState({ content: "" });
+      })
+      .catch(error => console.log(error.message));
+  }
+
+  editNote(note_id) {
+    db.ref(`all_notes/${this.state.user.uid}/${note_id}`)
+      .once("value")
+      .then(snapshot => {
+        this.setState({
+          note: snapshot.val(),
+          content: snapshot.val().content
+        });
+      });
+  }
+
+  deleteNote(note_id) {
+    db.ref(`all_notes/${this.state.user.uid}/${note_id}`).remove();
+  }
 
   render() {
     return (
       <div>
         <Header />
-        <div>
+        {this.state.notes.map(note => {
+          return (
+            <div key={note.note_id} className="card card-body shadow-sm m-4">
+              <p>{note.content}</p>
+              <button
+                className="btn btn-sm text-info"
+                onClick={() => this.editNote(note.note_id)}
+              >
+                Edit
+              </button>
+              <button
+                className="btn btn-sm text-danger"
+                onClick={() => this.deleteNote(note.note_id)}
+              >
+                Delete
+              </button>
+            </div>
+          );
+        })}
+        <div className="form-group mx-4">
+          <input
+            className="form-control"
+            onChange={this.handleChange}
+            value={this.state.content}
+          />
+          <button className="btn btn-success mt-3" onClick={this.createNote}>
+            Create Note
+          </button>
+        </div>
+
+        <div className="mx-4">
           Login in as: <strong>{this.state.user.email}</strong>
         </div>
         <Footer />
--- src/services/firebase.js
+++ src/services/firebase.js
@@ -10,3 +10,4 @@ const config = {
 firebase.initializeApp(config);
 
 export const auth = firebase.auth;
+export const db = firebase.database();
